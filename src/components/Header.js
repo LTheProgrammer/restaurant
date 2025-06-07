7211a4c52f1116431dc1c8cfb8c08b5271ad3c29
Louis <louisphilippemusto@gmail.com>
2025-06-07 13:23:54 -0400

    cleaning + basic structure

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import NavBar from "./Nav.";
+
+const Header = () => {
+    return (
+        <>
+            Header
+            <nav>
+                <NavBar />
+            </nav>
+        </>
+    );
+};
+
+export default Header;
